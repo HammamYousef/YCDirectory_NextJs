fdb8570da551d53eec6bb8959c0f0a0f445c9c06
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-30 10:59:03 +0300

    build the startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,25 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+const posts = [{
+  _id: "1",
+  title: "Innovative Tech Startup",
+  description: "A startup that revolutionizes technology.",
+  image: "/images/startup1.jpg",
+  _createdAt: "2023-10-01T12:00:00Z",
+  views: 150,
+  author: {
+    _id: "author1",
+    name: "John Doe",
+    image: "/images/author1.jpg",
+  },
+  category: "Technology",
+}]
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
   return (
+    <>
     <section className="pink_container">
         <h1 className="heading">
           Pitch Your Startup, <br />
@@ -16,5 +33,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
 
         <SearchForm query={query} />
     </section>
+
+    <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
