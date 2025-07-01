98169400b97bbac4531c82793af1d0199b5983be
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-07-01 15:08:53 +0300

    create a session

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -11,8 +11,7 @@ import View from '@/components/View';
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-
-  
+ 
   const post = await client.fetch(STARTUP_BY_ID_QUERY, {id})
   
   if (!post) return notFound();
